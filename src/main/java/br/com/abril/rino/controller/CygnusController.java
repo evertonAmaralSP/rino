1e1aec33aee640a35a2a9359496e6d3a133e35fb
Everton <everton.amaral@abril.com.br>
2012-04-19 20:48:06 -0300

    add lib gson para trabalhat com json/obj

--- src/main/java/br/com/abril/rino/controller/CygnusController.java
+++ src/main/java/br/com/abril/rino/controller/CygnusController.java
@@ -3,26 +3,38 @@ package br.com.abril.rino.controller;
 import org.apache.log4j.Logger;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.document.mongodb.MongoOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.gson.Gson;
+
+import br.com.abril.rino.model.Message;
+
 @Controller
 @RequestMapping("/cygnus")
 public class CygnusController {
 	
 	@Autowired AmqpTemplate amqpTemplate;
+	@Autowired MongoOperations mongoOperation;
+	@Autowired Gson gson;
 	
 	//get log4j handler
 	private static final Logger logger = Logger.getLogger(CygnusController.class);
 	
-	@RequestMapping(value="/{name}", method = RequestMethod.GET)
-	public String getMovie(@PathVariable String name, ModelMap model) {
+	@RequestMapping(value="/{categoria}/{action}/{label}", method = RequestMethod.GET)
+	public String getMovie(@PathVariable String categoria,@PathVariable String action,@PathVariable String label, ModelMap model) {
 
-		model.addAttribute("name", name);
-		 amqpTemplate.convertAndSend("hipo.queue", name);
+		model.addAttribute("nome", categoria);
+		
+		Message obj = new Message(categoria, action, label);  
+//		String json = categoria;
+		String json = gson.toJson(obj);
+        amqpTemplate.convertAndSend("hipo.queue", json);
+		 
 		//log it via log4j
 		if(logger.isDebugEnabled()){
 			logger.debug(model);
@@ -35,8 +47,13 @@ public class CygnusController {
 	@RequestMapping(value="cosumidor", method = RequestMethod.GET)
 	public String getMovie(ModelMap model) {
 
-		model.addAttribute("name", " COSUMIDOR "+ (String)amqpTemplate.receiveAndConvert("hipo.queue"));
-		
+		String receive = (String)amqpTemplate.receiveAndConvert("hipo.queue");
+		model.addAttribute("name", " COSUMIDOR "+ receive);
+		Message msg = gson.fromJson(receive, Message.class);
+        
+        //save
+        mongoOperation.save("profile",msg);
+        
 		return "list";
 
 	}
--- src/main/java/br/com/abril/rino/model/Message.java
+++ src/main/java/br/com/abril/rino/model/Message.java
@@ -1,13 +1,42 @@
 package br.com.abril.rino.model;
 
-public class Message {
-    private String value;
+import java.io.Serializable;
 
-    public String getValue() {
-        return value;
-    }
+public class Message implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String categoria;
+    private String action;
+    private String label;
+	
+    public Message() { super(); }
+    
+	public Message(String categoria, String action, String label) {
+		super();
+		this.categoria = categoria;
+		this.action = action;
+		this.label = label;
+	}
 
-    public void setValue(String value) {
-        this.value = value;
-    }
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+    
 }
